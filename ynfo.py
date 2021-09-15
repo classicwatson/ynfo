d67b1ff69397aa6124a2a3309f07671c68ca086e
classicwatson <6981304+classicwatson@users.noreply.github.com>
2021-09-15 17:53:29 -0400

    Time 'module' added. Improved spacing with padding.

--- ynfo.py
+++ ynfo.py
@@ -1,6 +1,15 @@
 # So far just a black screen
 # Soon, something else. Maybe.
 from tkinter import *
+from time import localtime, strftime
+
+def liveTimeDate():
+    time = strftime("%-I:%M:%S%p", localtime())
+    date = strftime("%a, %b %d, %Y", localtime())
+    timeModule.config(text=time.lower())
+    dateModule.config(text=date)
+    root.after(1000, liveTimeDate)
+
 
 root = Tk()
 
@@ -17,30 +26,38 @@ root.rowconfigure(1, weight=1)
 root.rowconfigure(3, weight=1)
 
 #3x3 frames layout
-topleft = Frame(root)
-topmid = Frame(root)
-topright = Frame(root)
-midleft = Frame(root)
-midmid = Frame(root)
-midright = Frame(root)
-bottomleft = Frame(root)
-bottommid = Frame(root)
-bottomright = Frame(root)
+topleft = Frame(root, bg='black')
+topmid = Frame(root, bg='black')
+topright = Frame(root, bg='black')
+midleft = Frame(root, bg='black')
+midmid = Frame(root, bg='black')
+midright = Frame(root, bg='black')
+bottomleft = Frame(root, bg='black')
+bottommid = Frame(root, bg='black')
+bottomright = Frame(root, bg='black')
 
 #populate frames
-time = Label(topleft, text='time', fg='white', bg='black', font=("Arial Bold", 70)).grid()
-weather = Label(topright, text='weather', fg='white', bg='black', font=("Arial Bold", 70)).grid()
-news = Label(bottommid, text='news goes here', fg='white', bg='black', font=("Arial Bold", 70)).grid()
+dateModule = Label(topleft, fg='white', bg='black', font=("Arial Bold", 16))
+timeModule = Label(topleft, fg='white', bg='black', font=("Arial Bold", 30))
+weatherModule = Label(topright, text='weather module goes here', fg='white', bg='black', font=("Arial Bold", 24))
+newsModule = Label(bottommid, text='news module goes here', fg='white', bg='black', font=("Arial Bold", 24))
+
+dateModule.grid()
+timeModule.grid()
+weatherModule.grid()
+newsModule.grid()
 
 #populate grid
-topleft.grid(row=0, column=0)
+topleft.grid(row=0, column=0, padx=45, pady=45)
 topmid.grid(row=0, column=2)
-topright.grid(row=0, column=4)
+topright.grid(row=0, column=4, padx=45, pady=45)
 midleft.grid(row=2, column=0)
 midmid.grid(row=2, column=2)
 midright.grid(row=2, column=4)
 bottomleft.grid(row=4, column=0)
-bottommid.grid(row=4, column=2)
+bottommid.grid(row=4, column=2, pady=45)
 bottomright.grid(row=4, column=4)
+
+liveTimeDate()
 
 root.mainloop()
